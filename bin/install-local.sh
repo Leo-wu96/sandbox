ca709a08cae3a5c5e49d0de997641ce4e8130fe2
Leo <leo15119602122.gmail.com>
2024-03-28 22:10:20 +0800

    hello world

--- bin/install-local.sh
+++ bin/install-local.sh
@@ -2,7 +2,8 @@
 
 # repeater's target dir
 REPEATER_TARGET_DIR=../target/repeater
-
+typeset HOME=/Users/wzx/desktop/sandbox/data/fll
+typeset SANDBOX_HOME=/Users/wzx/desktop/sandbox/data/fll/sandbox
 # exit shell with err_code
 # $1 : err_code
 # $2 : err_msg
@@ -13,11 +14,12 @@ exit_on_err()
 }
 
 # package
-sh ./package.sh || exit_on_err 1 "install failed cause package failed"
+# sh ./package.sh || exit_on_err 1 "install failed cause package failed"
 
 # extract sandbox to ${HOME}
 curl -s https://github.com/alibaba/jvm-sandbox-repeater/releases/download/v1.0.0/sandbox-1.3.3-bin.tar | tar xz -C ${HOME} || exit_on_err 1 "extract sandbox failed"
+#curl -s http://sandbox-ecological.oss-cn-hangzhou.aliyuncs.com/sandbox-1.2.1-bin.tar | tar xz -C ${HOME} || exit_on_err 1 "extract sandbox failed"
 
 # copy module to ~/.sandbox-module
 mkdir -p ${HOME}/.sandbox-module || exit_on_err 1 "permission denied, can not mkdir ~/.sandbox-module"
-cp -r ${REPEATER_TARGET_DIR}/* ${HOME}/.sandbox-module  || exit_on_err 1 "permission denied, can not copy module to ~/.sandbox-module"
+cp -r ${REPEATER_TARGET_DIR}/* ${SANDBOX_HOME}/sandbox-module || exit_on_err 1 "permission denied, can not copy module to ~/.sandbox-module"
